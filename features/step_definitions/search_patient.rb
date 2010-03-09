97f9f66cf437d6395cf910ff1635f9bf10eb0e8e
amunra <amunra@ubuntu.(none)>
2010-03-09 22:38:09 +0800

    checkbox solved, added a search function???

---
+++ features/step_definitions/search_patient.rb
@@ -0,0 +1,7 @@
+When /I search patient/ do
+  And "I click \"TODAY'S PATIENTS\""
+  And "I fill in \"last\" with \"Makiling\""
+  And "I press \"Search\""
+  And "I choose \"consult_patient_id\""
+  And "I press \"Select Patient\""
+end
